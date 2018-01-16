a732d19bea82c60cd3d347a2153422e839005a23
Ali Khalilli <akhalilli@users.noreply.github.com>
2018-01-16 22:45:15 +0400

    Create Juggling.py

---
+++ Arrays/Juggling.py
@@ -0,0 +1,44 @@
+"""
+@github: github.com/akhalilli
+
+Time Complexity: O(n)
+Space Complexity: O(1)
+"""
+
+
+def leftRotate(arr, d, n):
+    for i in range(gcd(d, n)):
+        temp = arr[i]
+        j = i
+        while 1:
+            k = (j + d) % n
+            if k == i:
+                break
+            arr[j] = arr[k]
+            j = k
+        arr[j] = temp
+
+
+"""
+
+1 2 3 4 5 6 7 8 9 10 11 12
+n=12
+d=3
+gcd = 3
+[1 2 3] [4 5 6] [7 8 9] [10 11 12]
+
+arr[0] - arr[3] - arr[6] - arr[9] - arr[13]
+
+[4 2 3] [7 5 6] [10 8 9] [1 11 12]
+
+[4 5 3] [7 8 6] [10 11 9] [1 2 12]
+
+[4 5 6] [7 8 9] [10 11 12] [1 2 3]
+"""
+
+# GCD = Greatest Common Divisor
+def gcd(a, b):
+    if b == 0:
+        return a
+    else:
+        return gcd(b, a % b)
