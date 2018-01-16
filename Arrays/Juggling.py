55107903fdea57e140714dbb1f2b6b6fcca74ab2
Ali Khalilli <akhalilli@users.noreply.github.com>
2018-01-16 22:46:39 +0400

    Update Juggling.py

--- Arrays/Juggling.py
+++ Arrays/Juggling.py
@@ -5,7 +5,6 @@ Time Complexity: O(n)
 Space Complexity: O(1)
 """
 
-
 def leftRotate(arr, d, n):
     for i in range(gcd(d, n)):
         temp = arr[i]
@@ -18,16 +17,15 @@ def leftRotate(arr, d, n):
             j = k
         arr[j] = temp
 
-
 """
 
 1 2 3 4 5 6 7 8 9 10 11 12
-n=12
-d=3
+n = 12
+d = 3
 gcd = 3
 [1 2 3] [4 5 6] [7 8 9] [10 11 12]
 
-arr[0] - arr[3] - arr[6] - arr[9] - arr[13]
+arr[0] - arr[3] - arr[6] - arr[9] - (9+3)%12
 
 [4 2 3] [7 5 6] [10 8 9] [1 11 12]
 
