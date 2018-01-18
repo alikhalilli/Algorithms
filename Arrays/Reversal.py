a780caffbce3e8e98cbc81081edc51c64f119ffa
Ali Khalilli <akhalilli@users.noreply.github.com>
2018-01-18 10:55:25 +0400

    Create Reversal.py

---
+++ Arrays/Reversal.py
@@ -0,0 +1,35 @@
+"""
+@github: github.com/akhalilli
+
+arr = [1, 2, 3, 4, 5]
+
+d = 2
+A = [1, 2]
+rA = [2, 1]
+B = [3, 4, 5]
+rB = [5, 4, 3]
+
+rArB = [2, 1, 5, 4, 3]
+r(rArB) = [3, 4, 5, 1, 2]
+"""
+
+
+
+def rotateByReverse(arr, d):
+	n = len(arr)
+	reverseArray(arr, 0, d-1)
+	reverseArray(arr, d, n-1)
+	reverseArray(arr, 0, n-1)
+	print(arr)
+
+
+def reverseArray(arr, start, end):
+	while start < end:
+		temp = arr[start]
+		arr[start] = arr[end]
+		arr[end] = temp
+		start += 1
+		end = end - 1
+
+arr = [1, 2, 3, 4, 5]
+rotateByReverse(arr, 2)
