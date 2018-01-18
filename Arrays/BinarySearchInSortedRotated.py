93d7a82af4b8d5429e5e1e46fa05691dcf82c5e1
Ali Khalilli <akhalilli@users.noreply.github.com>
2018-01-18 14:08:22 +0400

    Update BinarySearchInSortedRotated.py

--- Arrays/BinarySearchInSortedRotated.py
+++ Arrays/BinarySearchInSortedRotated.py
@@ -6,7 +6,6 @@ def binarySearch(arr, low, high, key):
 	if high < low:
 		return -1
 
-    # low + int((high - low)/2)
     mid = int(low + (high - low) / 2)
 
     if key == arr[mid]:
@@ -45,13 +44,15 @@ def findPivot(arr, low, high):
 def pivotedBinarySearch(arr, n, key):
  
     pivot = findPivot(arr, 0, n-1);
-    
+ 
+ 	# not pivoted at all
     if pivot == -1:
         return binarySearch(arr, 0, n-1, key);
-    
+
     if arr[pivot] == key:
         return pivot
-        
+
     if arr[0] <= key:
         return binarySearch(arr, 0, pivot-1, key);
+
     return binarySearch(arr, pivot+1, n-1, key);
