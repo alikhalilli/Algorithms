6d7b65d584f0a3743e2820ee27d24746282d999f
Ali Khalilli <12389297+akhalilli@users.noreply.github.com>
2018-06-10 23:23:27 +0400

    reversal left and right

--- Arrays/Rotation/Reversal.py
+++ Arrays/Rotation/Reversal.py
@@ -13,26 +13,39 @@ A = [1, 2]
 B = [3, 4, 5, 6]
 rA = [2, 1]
 rB = [6, 5, 4, 3]
+
 rArB = [2, 1, 6, 5, 4, 3]
 r(rArB) = [3, 4, 5, 6, 1, 2]
 
-
+[5, 6, 1, 2, 3, 4]
+[4, 3, 2, 1, 6, 5]
+rB = [6, 5]
+rA = [4, 3, 2, 1]
+rArB = [4, 3, 2, 1, 6, 5]
+r(rArB) = [5, 6, 1, 2, 3, 4]
 [1, 2, 3, 4, 5, 6]
 
 """
 
 
 class Solution(object):
-    def __init__(self, arr, d, printarr=True):
+    def __init__(self, arr, direction, d, printarr=True):
         self.arr = arr
         self.n = len(arr)
         self.d = d % self.n
         self.print = printarr
+        self.direction = direction
 
     def rotateArr(self):
-        self.__reverseArr(0, self.d-1)
-        self.__reverseArr(self.d, self.n-1)
-        self.__reverseArr(0, self.n-1)
+        if self.direction == 'left':
+            self.__reverseArr(0, self.d-1)
+            self.__reverseArr(self.d, self.n-1)
+            self.__reverseArr(0, self.n-1)
+        else:
+            self.__reverseArr(self.n-self.d, self.n-1)
+            self.__reverseArr(0, self.n-self.d-1)
+            self.__reverseArr(0, self.n-1)
+
         if self.print:
             print(self.arr)
 
@@ -46,5 +59,5 @@ class Solution(object):
 
 
 arr = [1, 2, 3, 4, 5, 6]
-sol = Solution(arr, 2)
+sol = Solution(arr, 'right', 2)
 sol.rotateArr()
