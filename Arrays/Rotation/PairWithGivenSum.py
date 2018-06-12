83b5f724513439045b9bbafd6fcd6d486ac12b9b
Ali Khalilli <12389297+akhalilli@users.noreply.github.com>
2018-06-13 00:26:03 +0400

    pair with given sum rotated array

--- Arrays/Rotation/PairWithGivenSum.py
+++ Arrays/Rotation/PairWithGivenSum.py
@@ -1,5 +1,29 @@
 """
 @github: github.com/alikhalilli
 
-arr = []
+arr = [4, 5, 6, 1, 2, 3]
 """
+
+
+def findPivot(arr):
+    n = len(arr)
+    for i in range(n):
+        if arr[i] > arr[i + 1]:
+            return i
+    return -1
+
+
+def findPair(arr, sum):
+    n = len(arr)
+    p = findPivot(arr)
+    l = (p + 1) % n  # min element's index
+    r = p  # max element's index
+    while l != r:
+        if arr[l] + arr[r] == sum:
+            return (l, r)
+
+        if (arr[l] + arr[r] < sum):
+            l = (l + 1) % n
+        else:
+            r = (n + r - 1) % n
+    return False
