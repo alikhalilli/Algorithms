5b23cc90426b0949ff646c924d75731844a7c4b7
Ali Khalilli <12389297+akhalilli@users.noreply.github.com>
2018-06-15 17:18:44 +0400

    insertion sort

---
+++ Sorting/InsertionSort.py
@@ -0,0 +1,22 @@
+"""
+@github: github.com/alikhalilli
+
+Time Complexity: O(n^2)
+Space Complexity: O(1)
+"""
+arr = [9, 8, 10, 5]
+
+
+def insertionSort(arr):
+    n = len(arr)
+    for i in range(n):
+        static_point = arr[i]
+        j = i - 1
+        while j >= 0 and arr[j] > static_point:
+            arr[j+1] = arr[j]
+            j = j-1
+        arr[j+1] = static_point
+    return arr
+
+
+print(insertionSort(arr))
---
+++ Sorting/RecursiveBubbleSort.py
@@ -0,0 +1,8 @@
+"""
+@github: github.com/alikhalilli
+
+arr = [9, 8, 10, 3, 5]
+
+Time Complexity: O(n^2)
+Space Complexity: O(1)
+"""
