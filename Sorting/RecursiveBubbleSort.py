ca438a999ff617ff534322e6bc6e003519520eab
Ali Khalilli <12389297+akhalilli@users.noreply.github.com>
2018-06-17 00:02:10 +0400

    updates

--- Sorting/RecursiveBubbleSort.py
+++ Sorting/RecursiveBubbleSort.py
@@ -19,10 +19,9 @@ def bubbleSort(arr, c_index):
         if arr[j] > arr[j+1]:
             arr[j], arr[j+1] = arr[j+1], arr[j]
         j += 1
-    print(arr)
 
 
-arr = [4, 7, 3, 5, 1]
-#arr = [1, 2, 3, 4, 5, 6, 7]
+#arr = [4, 7, 3, 5, 1]
+arr = [1, 2, 3, 4, 5, 6, 7]
 bubbleSort(arr, len(arr)-1)
 print(arr)
